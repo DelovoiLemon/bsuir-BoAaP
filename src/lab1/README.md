74352fcee7ffa59ce8ff7ae673e95c66b97963aa
Delovoilemon <dima.troshkin.ru@gmail.com>
2023-09-15 21:57:44 +0300

    Move labs to src dir

---
+++ src/lab1/README.md
@@ -0,0 +1,34 @@
+# Тема №1. Линейный вычислительный процесс
+## Первый уровень сложности
+[Исходный код](task1.c)
+### Пример
+```
+➜  lab1 git:(master) ✗ gcc -lm task1.c -o task1
+➜  lab1 git:(master) ✗ ./task1
+Enter alpha: 55
+Result: z1 = -45.183088
+Result: z2 = -45.182924
+➜  lab1 git:(master) ✗ 
+```
+## Второй уровень сложности
+[Исходный код](task2.c)
+### Пример
+```
+➜  lab1 git:(master) ✗ gcc -lm task2.c -o task2
+➜  lab1 git:(master) ✗ ./task2 
+Enter alpha: 23
+Result: z1 = -0.619038
+Result: z2 = -0.619038
+➜  lab1 git:(master) ✗ 
+```
+
+## Третий уровень сложности
+[Исходный код](task3.c)
+### Пример
+```
+➜  lab1 git:(master) ✗ gcc -lm task3.c -o task3
+➜  lab1 git:(master) ✗ ./task3
+Enter x, y, z: 6.251 0.827 25.001
+Result: b = 0.712021
+➜  lab1 git:(master) ✗ 
+```
---
+++ src/lab1/task3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+#define DIVBYZEROMSG "Error! Zero division!\n"
+#define NEGATIVEINSQRTMSG "Error! Negative number in parameter of sqrt!"
+#define NONUMMSG "Error! No all numbers entered!\nPlease enter 3 parameters."
+#define PI 	3.1415926
+#define MATH_E	2.7182818
+
+double b(double x, double y, double z) {
+	if(x + y < 0) {
+		printf(NEGATIVEINSQRTMSG);
+		exit(1);
+	}
+	if(pow(MATH_E, abs(x-y) + x/2) == 0){
+		printf(DIVBYZEROMSG);
+		exit(1);
+	}
+	return 	pow(y, pow(abs(x), 1.0/3)) + 
+		pow(cos(y), 3) *
+		(abs(x-y)*(1+pow(sin(z), 2))/ sqrt(x+y)) /
+		(pow(MATH_E, abs(x - y))+ x/2);
+}
+
+
+int main() {
+	double x, y, z;
+	
+	printf("Enter x, y, z: ");
+
+	if(scanf("%lf %lf %lf", &x, &y, &z) < 3) {
+		printf(NONUMMSG);
+		exit(1);
+	}
+
+	printf("Result: b = %f\n", b(x, y, z));	
+
+	return 0;
+}

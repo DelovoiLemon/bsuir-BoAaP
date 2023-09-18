fa970510aeded0e8ad6d9ec6910ed1d7ce2a230f
Delovoilemon <dima.troshkin.ru@gmail.com>
2023-09-18 21:57:15 +0300

    Finish lab3 and lab2

--- labs/lab3/src/task3.c
+++ labs/lab3/src/task3.c
@@ -3,28 +3,28 @@
 #include<stdlib.h>
 #define NONUMMSG "Error! No number entered!\n"
 
-long fact(long num){
-	return num > 1 ? num * fact(num - 1) : 1;
-}
+double my_abs(double n) { return n > 0 ? n : -n; }
+long fact(long num){ return num > 1 ? num * fact(num - 1) : 1; }
 
 double S(double x, long n) {
 	double sum = 0;
-	for(long k = 0; k < n; k++) {
-		sum += (k*k + 1)/fact(k) * pow(x/2, k);
-	}
+	for(long k = 0; k <= n; k++) 
+		sum += (k*k + 1.)/fact(k) * pow(x/2, k);
+	return sum;
 }
 
-double Y(double x) {
-	return (x*x/4 + x/2 + 1) * exp(x/2);
-}
+double Y(double x) { return (x*x/4 + x/2 + 1) * exp(x/2); }
 
 int main() {
-	double x, y, a;
-	printf("Enter x, y, a: ");
-	if(scanf("%lf %lf %lf", &x, &y, &a) < 3) {
+	double x, e; int n;
+	printf("Enter x, e: ");
+	if(scanf("%lf %lf", &x, &e) < 2) {
 		printf(NONUMMSG);
 		exit(1);
 	}
-	printf("Result: m = %f\n", m(x, y, a));
+	for(n = 1; my_abs(S(x, n) - Y(x)) >= e; n++) {
+		printf("%lf\t%lf\t%lf\n", S(x, n), Y(x), my_abs(S(x, n) - Y(x)));
+	}
+	printf("Delta = %lf\nResult n = %i\n", my_abs(S(x, n) - Y(x)), n);
 	return 0;
 }
